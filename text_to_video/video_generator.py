73a5855e2663364971c72bf24989fc7e9b7b4a37
sonu4sam <sonu4sam@outlook.com>
2023-08-17 23:21:09 -0500

    aider: Replaced all the comments in `VideoGenerator`.
    # Context:
    USER: replace all the comments in `VideoGenerator`

--- text_to_video/video_generator.py
+++ text_to_video/video_generator.py
@@ -6,13 +6,13 @@ class VideoGenerator:
         return video
 
     def convert_text_to_audio(self, text: str) -> str:
-        # Implement logic to convert text to audio
+        # TODO: Implement logic to convert text to audio
         pass
 
     def generate_video_frames(self, audio: str) -> str:
-        # Implement logic to generate video frames from audio
+        # TODO: Implement logic to generate video frames from audio
         pass
 
     def combine_video_frames(self, frames: str) -> str:
-        # Implement logic to combine video frames into a video
+        # TODO: Implement logic to combine video frames into a video
         pass
